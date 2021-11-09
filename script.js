a8d4e61cc6f272a3f6935afc6239b98e1280226d
Riley-Hoffman <hoffman.riley@gmail.com>
2021-11-08 19:50:42 -0500

    added function to clear #main from url. Added styling to skip-link.

--- script.js
+++ script.js
@@ -109,6 +109,7 @@ weatherApp.displayTodaysData = (todaysDataFromApi) => {
 
     // Publish results to the page
     weatherApp.resultsDiv.appendChild(weatherApp.p);
+    cleanUrl()
 }
 
 // Function to display weather data on the page
@@ -124,7 +125,13 @@ weatherApp.displayForecastData = (forecastDataFromApi) => {
         forecastP.textContent = `${Math.round(day['temp'])}° C`
         li.appendChild(forecastP);
         weatherApp.forecastOl.appendChild(li)
+        cleanUrl()
     })
+}
+
+// Remove #main from url if links to #main (skip to, header arrow) are clicked
+function cleanUrl () {
+history.pushState("", document.title, window.location.pathname + window.location.search);
 }
 
 weatherApp.init = () => {
@@ -136,6 +143,20 @@ weatherApp.init = () => {
 // Kickoff the app 🏈
 weatherApp.init();
 
+
+// var scrollV, scrollH, loc = window.location;
+// if ("pushState" in history)
+//     history.pushState("", document.title, loc.pathname + loc.search);
+// else {
+//     // Prevent scrolling by storing the page's current scroll offset
+//     scrollV = document.body.scrollTop;
+//     scrollH = document.body.scrollLeft;
+
+//     loc.hash = "";
+
+//     // Restore the scroll offset, should be flicker free
+//     document.body.scrollTop = scrollV;
+//     document.body.scrollLeft = scrollH;
 
 /* PSEUDO CODE
 // App functionality thinking:
